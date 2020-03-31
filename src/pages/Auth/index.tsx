0cd006814c420045745fb7655201e1e476199dd6
Lemii <lemiidelegate@gmail.com>
2020-03-31 13:32:34 +0200

    feat: password system (#8)
    * feat: add hash functionality
    
    * ui: add icons
    
    * feat: add hash storage functions
    
    * feat: implement password system
    
    * chore: remove unused dependency

---
+++ src/pages/Auth/index.tsx
@@ -0,0 +1,100 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Redirect } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+import { PasswordContext } from '../../contexts';
+import {
+  getPasswordHash,
+  setPasswordHash,
+  removePasswordHash,
+  clearNodePasswords
+} from '../../utils/storage';
+import { hash } from '../../utils/crypto';
+
+export default function Auth(): JSX.Element {
+  const [password, setPassword] = useState<string>('');
+  const [redirect, setRedirect] = useState<boolean>(false);
+
+  const context = useContext(PasswordContext);
+
+  useEffect(() => {
+    document.title = 'Authorize | Lisk Manager';
+  }, []);
+
+  const handleSubmit = (e: React.SyntheticEvent): void => {
+    e.preventDefault();
+
+    const storedHash = getPasswordHash();
+    const inputHash = hash(password);
+
+    if (!storedHash || inputHash === storedHash) {
+      toast.success('Succesfully authorized');
+
+      if (!storedHash) {
+        setPasswordHash(inputHash);
+      }
+
+      context.update(password);
+      setRedirect(true);
+      return;
+    }
+
+    toast.error('Password mismatch, could not authorize');
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const { value } = e.target;
+
+    setPassword(value);
+  };
+
+  const handleReset = (): void => {
+    setPassword('');
+
+    clearNodePasswords();
+    removePasswordHash();
+
+    toast.info('All password data removed successfully');
+  };
+
+  return (
+    <div className="container text-light">
+      <h5 className="mb-5">Authorize</h5>
+
+      <form onSubmit={handleSubmit}>
+        <div className="form-group mb-5">
+          <input type="text" className="hidden" autoComplete="application" readOnly />
+          <label htmlFor="app-password">
+            <strong>Enter password</strong>
+          </label>
+          <input
+            name="app-password"
+            value={password}
+            onChange={handleChange}
+            type="password"
+            className="form-control"
+            id="app-password"
+            aria-describedby="app-password"
+            placeholder="*******************"
+            autoComplete="new-password"
+          />
+          <small id="app-password-help" className="form-text text-muted">
+            Used to encrypt and decrypt sensitive data. Read more <Link to="/faq">here</Link>.
+          </small>
+        </div>
+
+        <button type="submit" className="btn btn-primary" disabled={!password.length}>
+          Proceed <FontAwesomeIcon icon="sign-in-alt" />
+        </button>
+
+        <button type="button" onClick={handleReset} className="btn btn-outline-primary ml-3">
+          Reset <FontAwesomeIcon icon="undo-alt" />
+        </button>
+      </form>
+
+      {redirect && <Redirect to="/" />}
+    </div>
+  );
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Overview from '../pages/Overview';
 import AddNode from '../pages/AddNode';
 import Settings from '../pages/Settings';
 import Faq from '../pages/Faq';
 import NotFound from '../pages/NotFound';
+import Auth from '../pages/Auth';
 
-const Routes: React.FC = () => (
-  <Switch>
-    <Route exact path="/" component={Overview} />
-    <Route exact path="/add" component={AddNode} />
-    <Route exact path="/settings" component={Settings} />
-    <Route exact path="/faq" component={Faq} />
-    <Route component={NotFound} />
-  </Switch>
-);
+import { PasswordContext } from '../contexts';
+
+const Routes: React.FC = () => {
+  const { password } = useContext(PasswordContext);
+
+  const PrivateRoute = ({ component: Component, ...rest }: any) => (
+    <Route
+      {...rest}
+      render={props => (password ? <Component {...props} /> : <Redirect to="/auth" />)}
+    />
+  );
+
+  return (
+    <Switch>
+      <Route exact path="/auth" component={Auth} />
+      <PrivateRoute exact path="/" component={Overview} />
+      <PrivateRoute exact path="/add" component={AddNode} />
+      <PrivateRoute exact path="/settings" component={Settings} />
+      <Route exact path="/faq" component={Faq} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+};
 
 export default Routes;
