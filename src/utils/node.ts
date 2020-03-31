6070ab3142b77b6f30b798dc8f525ed0b5b060bf
Lemii <lemiidelegate@gmail.com>
2020-03-31 14:47:54 +0200

    refactor: utilize single entry point

---
+++ src/utils/node.ts
@@ -0,0 +1,33 @@
+import axios, { AxiosResponse } from 'axios';
+
+import { decrypt } from './crypto';
+import { INode, INodeStatus, IForgingStatus } from '../interfaces';
+
+export const fetchNodeStatus = (node: string): Promise<INodeStatus> =>
+  axios.get(node + '/api/node/status').then(res => res.data.data);
+
+export const fetchForgingStatus = (node: string) => axios.get(node + '/api/node/status/forging');
+
+export const toggleForgingStatus = (
+  node: INode,
+  checked: boolean
+): Promise<AxiosResponse<IForgingStatus>> =>
+  axios.put(node.ip + '/api/node/status/forging', {
+    publicKey: node.pubkey,
+    password: decrypt(node.password!, node.id),
+    forging: checked
+  });
+
+export const fetchNodeVersion = async (node: string): Promise<string> => {
+  const nodeStatus = await fetchNodeStatus(node);
+
+  if ('chainMaxHeightFinalized' in nodeStatus) {
+    return '3';
+  }
+
+  if ('consensus' in nodeStatus) {
+    return '2';
+  }
+
+  return '0';
+};
