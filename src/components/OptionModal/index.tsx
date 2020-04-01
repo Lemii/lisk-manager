2dae63e24bc5b5990cfb57c8099e1526101713d7
Lemii <lemiidelegate@gmail.com>
2020-04-01 13:19:47 +0200

    feat: add confirm modals (#14)
    * feat: add confirm modal
    
    * refactor: streamline option modal
    
    * feat: add confirm modals

--- src/components/OptionModal/index.tsx
+++ src/components/OptionModal/index.tsx
@@ -55,54 +55,33 @@ export default function OptionModal({
   return (
     <Modal show={show} onHide={handleClose} className="text-light">
       <form onSubmit={handleSubmit}>
-        {type !== 'deletion' ? (
-          <Modal.Body>
-            <label htmlFor={name}>
-              <h4>{body}</h4>
-            </label>
+        <Modal.Body>
+          <label htmlFor={name}>
+            <h4>{body}</h4>
+          </label>
 
-            <input
-              value={input}
-              onChange={handleChange}
-              type={type}
-              className="form-control mb-4"
-              name={name}
-              id={name}
-            />
+          <input
+            value={input}
+            onChange={handleChange}
+            type={type}
+            className="form-control mb-4"
+            name={name}
+            id={name}
+          />
 
-            <div className="text-right">
-              <button
-                className="btn btn-sm btn-outline-primary mr-2"
-                onClick={handleClose}
-                type="button"
-              >
-                Close
-              </button>
-              <button className="btn btn-sm btn-primary" type="submit">
-                Save <FontAwesomeIcon icon="save" />
-              </button>
-            </div>
-          </Modal.Body>
-        ) : (
-          <Modal.Body>
-            <label htmlFor={name}>
-              <h4>{body}</h4>
-            </label>
-
-            <div className="text-right">
-              <button
-                className="btn btn-sm btn-outline-primary mr-2"
-                onClick={handleClose}
-                type="button"
-              >
-                Cancel
-              </button>
-              <button className="btn btn-sm btn-primary" type="submit">
-                Remove <FontAwesomeIcon icon="trash-alt" />
-              </button>
-            </div>
-          </Modal.Body>
-        )}
+          <div className="text-right">
+            <button
+              className="btn btn-sm btn-outline-primary mr-2"
+              onClick={handleClose}
+              type="button"
+            >
+              Close
+            </button>
+            <button className="btn btn-sm btn-primary" type="submit">
+              Save <FontAwesomeIcon icon="save" />
+            </button>
+          </div>
+        </Modal.Body>
       </form>
     </Modal>
   );
