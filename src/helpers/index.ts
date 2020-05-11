43561de644b705c50858adc93e6cbda9ceb3900f
Lemii <lemiidelegate@gmail.com>
2020-05-11 10:49:03 +0200

    feat: add byte formatter

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -3,3 +3,15 @@ import moment from 'moment';
 export const formatNumber = (input: number): string => input.toLocaleString();
 
 export const formatDate = (input: number): string => moment(input).format('LTS');
+
+export const formatBytes = (bytes: number, decimals: number = 2): string => {
+  if (bytes === 0) return '0 Bytes';
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+};
